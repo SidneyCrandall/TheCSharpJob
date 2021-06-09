0a04e958039c5b6ac482f1aa205f002f8f64eaa7
Sidney Crandall <sdncrandall5@gmail.com>
2021-06-09 15:45:44 -0500

    Recon mission complete

--- Bank.cs
+++ Bank.cs
@@ -8,13 +8,13 @@ namespace HeistPartII
         public int CashOnHand { get; set; }
         public int AlarmScore { get; set; } 
         public int VaultScore { get; set; }
-        public int SecurtityGuardSccore { get; set; }
+        public int SecurityGuardSccore { get; set; }
         // A computed boolean property called IsSecure. If all the scores are less than or equal to 0, this should be false. If any of the scores are above 0, this should be true
         public bool IsSecure
         {
             get
             {
-                if (CashOnHand <= 0 && VaultScore <= 0 && SecurtityGuardSccore <= 0)
+                if (CashOnHand <= 0 && VaultScore <= 0 && SecurityGuardSccore <= 0)
                 {
                     return false;
                 }
